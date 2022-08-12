c90fca5304762a64350d8efd1b3eed0a463d5849
Suracheth Chawla <dev1987@gmail.com>
2022-08-12 22:22:58 +0700

    Add accepting number from user and displaying the value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    // we can use sto::io::stdin if we do not import
+    // std::io by `use std::io` on the first line
+    // or we could write
+    //io::stdin().read_line(&mut guess).expect("Failed to read line");
+    // on a single line
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+    // or you can write `println!("You guessed: {}", guess)`
 }
